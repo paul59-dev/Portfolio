6efcb771979c5ffdbbbb83a707f0fe8e23083b43
Paul-developpeur <paulallebee@gmail.com>
2024-01-22 00:01:41 +0100

    feat: create page of skills

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -22,7 +22,7 @@ const About = () => {
             <section>
                 <h2>Formations</h2>
                 <div className="formations">
-                    <Formations title="RNCP niveau 6 CDNT" institution="Aston By SQLI" information="Le titre RNCP au niveau CDNT certifie les compétences du professionnel en tant que Concepteur Développeur de Nouvelles Technologies, démontrant ainsi son expertise dans la création et le développement de solutions innovantes." year="2023 - 2025" />
+                    <Formations title="RNCP niveau 6 CDNT" institution="Aston By SQLI" information="Le titre RNCP au niveau CDNT atteste des compétences du professionnel en tant que Concepteur Développeur de Nouvelles Technologies. Il certifie son expertise dans la conception et le développement de solutions innovantes, démontrant ainsi sa maîtrise des technologies émergentes." year="2023 - 2025" />
                     <Formations title="BTS SNIR Option A" institution="Lycée de l'Europe" information="Le BTS SNIR, Systèmes Numériques en Informatique et Réseaux, prépare les étudiants à devenir des professionnels compétents dans la conception et le développement de systèmes informatiques et réseaux, offrant ainsi une expertise pointue dans les nouvelles technologies." year="2021 - 2023" />
                     <Formations title="BAC STI2D Option SIN" institution="Lycée de l'Europe" information="Le bac STI2D avec l'option SIN (Systèmes d'Information et Numérique) propose une spécialisation approfondie dans les technologies de l'information et du numérique, offrant aux étudiants une expertise pointue dans la conception et la gestion des systèmes d'information." year="2018 - 2021" />
                 </div>
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,13 +1,87 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Header from '../components/Header';
 import Navigation from '../components/Navigation';
 
 const Skills = () => {
+    const [language, setLanguage] = useState([]);
+    const [framework, setFramework] = useState([]);
+    const [system, setSystem] = useState([]);
+    const [otherSkill, setOtherSkill] = useState([]);
+
+    useEffect(() => {
+        axios.get("/data/language.json")
+        .then((res) => setLanguage(res.data))
+        .catch((error) => {
+            console.error('Erreur lors de la récupération des données:', error);
+        });
+    }, []);
+
+    useEffect(() => {
+        axios.get("/data/frameworks.json")
+        .then((res) => setFramework(res.data))
+        .catch((error) => {
+            console.error('Erreur lors de la récupération des données:', error);
+        });
+    }, []);
+
+    useEffect(() => {
+        axios.get("/data/system.json")
+        .then((res) => setSystem(res.data))
+        .catch((error) => {
+            console.error('Erreur lors de la récupération des données:', error);
+        });
+    }, []);
+
+    useEffect(() => {
+        axios.get("/data/otherSkills.json")
+        .then((res) => setOtherSkill(res.data))
+        .catch((error) => {
+            console.error('Erreur lors de la récupération des données:', error);
+        });
+    }, []);
     return (
         <div className="skills">
             <Header/>
             <Navigation/>
-            <p>Language programing</p>
+            <section>
+                <h2>Langages de programmations</h2>
+                <div className="grid-container">
+                    {language.map((item, index) => (
+                        <div key={index} className="card">
+                            <img src={item.image} alt={item.alt} />
+                            <p>{item.title}</p>
+                        </div>
+                    ))}
+                </div>
+                <h2>Frameworks</h2>
+                <div className="grid-container">
+                    {framework.map((item, index) => (
+                        <div key={index} className="card">
+                            <img src={item.image} alt={item.alt} />
+                            <p>{item.title}</p>
+                        </div>
+                    ))}
+                </div>
+                <h2>Systèmes d'exploitations</h2>
+                <div className="grid-container">
+                    {system.map((item, index) => (
+                        <div key={index} className="card">
+                            <img src={item.image} alt={item.alt} />
+                            <p>{item.title}</p>
+                        </div>
+                    ))}
+                </div>
+                <h2>Autres compétences</h2>
+                <div className="grid-container">
+                    {otherSkill.map((item, index) => (
+                        <div key={index} className="card">
+                            <img src={item.image} alt={item.alt} />
+                            <p>{item.title}</p>
+                        </div>
+                    ))}
+                </div>
+            </section> 
         </div>
     );
 };
