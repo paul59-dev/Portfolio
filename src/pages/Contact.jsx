17850bd4b9eda13dcc22e3b34343b10827fe7fe9
Paul-developpeur <paulallebee@gmail.com>
2024-01-22 16:11:09 +0100

    feat: add contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import Header from '../components/Header';
 import Navigation from '../components/Navigation';
+import FacebookIcon from "../assets/images/facebook.png"
+import InstagramIcon from "../assets/images/instagram.png"
+import TreadsIcon from "../assets/images/threads.png"
+import XIcon from "../assets/images/twitterpng.png"
 
 const Contact = () => {
     return (
         <div className="contact">
             <Header/>
             <Navigation/>
-            <p>contact</p>
+            <section>
+                <h2>Contactez-moi</h2>
+                <div className="network-social">
+                    <h3>Réseaux sociaux :</h3>
+                    <a href="https://www.facebook.com/gugux.feu" target="_blank" rel="noopener noreferrer">
+                        <img src={FacebookIcon} alt="Contacte moi sur facebook" />
+                    </a>
+                    <a href="https://www.instagram.com/mr_paul59/" target="_blank" rel="noopener noreferrer">
+                        <img src={InstagramIcon} alt="Contacte moi sur instagram" />
+                    </a>
+                    <a href="https://www.threads.net/@mr_paul59" target="_blank" rel="noopener noreferrer">
+                        <img src={TreadsIcon} alt="Contacte moi sur treads" />
+                    </a>
+                    <a href="https://twitter.com/MrRaccoon59" target="_blank" rel="noopener noreferrer">
+                        <img src={XIcon} alt="Contacte moi sur x" />
+                    </a>
+                </div>
+            </section>
         </div>
     );
 };
