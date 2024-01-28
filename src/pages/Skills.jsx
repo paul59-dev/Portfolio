c22b33f14796fed9673d501eff21b741693240df
Paul-developpeur <paulallebee@gmail.com>
2024-01-28 21:19:19 +0100

    fix: download image in local

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -2,12 +2,38 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Header from '../components/Header';
 import Navigation from '../components/Navigation';
+// Language
+import html from '../assets/images/languages/html5.png';
+import css from '../assets/images/languages/css-3.png';
+import js from '../assets/images/languages/javascript.png';
+import java from '../assets/images/languages/java.png';
+import go from '../assets/images/languages/go.png';
+import ruby from '../assets/images/languages/rubygems.png';
+import php from '../assets/images/languages/php.png';
+import python from '../assets/images/languages/python.png';
+//Framewoks
+import react from '../assets/images/frameworks/react.png';
+import ror from '../assets/images/frameworks/ruby-on-rails.png';
+import node from '../assets/images/frameworks/nodejs.png';
+// OS
+import windows from '../assets/images/systems/windows.png';
+import linux from '../assets/images/systems/Linux.png';
+import android from '../assets/images/systems/android.png';
+// Other
+import support from '../assets/images/otherSkills/support.webp';
+import docker from '../assets/images/otherSkills/docker.png';
+import cisco from '../assets/images/otherSkills/cisco.png';
 
 const Skills = () => {
     const [language, setLanguage] = useState([]);
     const [framework, setFramework] = useState([]);
     const [system, setSystem] = useState([]);
     const [otherSkill, setOtherSkill] = useState([]);
+
+    let languageArray = [html, css, js, java, go, ruby, php, python];
+    let frameworkArray = [react, ror, node];
+    let osArray = [windows, linux, android];
+    let otherSkillArray = [support, docker, cisco];
 
     useEffect(() => {
         axios.get("/data/language.json")
@@ -49,7 +75,7 @@ const Skills = () => {
                 <div className="grid-container">
                     {language.map((item, index) => (
                         <div key={index} className="card">
-                            <img src={item.image} alt={item.alt} />
+                            <img src={languageArray[index]} alt={item.alt} />
                             <p>{item.title}</p>
                         </div>
                     ))}
@@ -58,7 +84,7 @@ const Skills = () => {
                 <div className="grid-container">
                     {framework.map((item, index) => (
                         <div key={index} className="card">
-                            <img src={item.image} alt={item.alt} />
+                            <img src={frameworkArray[index]} alt={item.alt} />
                             <p>{item.title}</p>
                         </div>
                     ))}
@@ -67,7 +93,7 @@ const Skills = () => {
                 <div className="grid-container">
                     {system.map((item, index) => (
                         <div key={index} className="card">
-                            <img src={item.image} alt={item.alt} />
+                            <img src={osArray[index]} alt={item.alt} />
                             <p>{item.title}</p>
                         </div>
                     ))}
@@ -76,7 +102,7 @@ const Skills = () => {
                 <div className="grid-container">
                     {otherSkill.map((item, index) => (
                         <div key={index} className="card">
-                            <img src={item.image} alt={item.alt} />
+                            <img src={otherSkillArray[index]} alt={item.alt} />
                             <p>{item.title}</p>
                         </div>
                     ))}
